5cf41ba0e7c7868ec0f2ec074ab3a9d6f7b43714
Rajat Negi <007negirajat@gmail.com>
2026-06-11 00:31:35 +0530

    code

---
+++ input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+int num;//define variable to store
+
+cout<<"enter a number:";
+cin>>num;
+
+cout<<"you entered "<<num;
+
+
+    return 0;
+}
